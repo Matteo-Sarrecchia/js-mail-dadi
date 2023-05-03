a8874cd74d6ee21dfd779d9a695914ab0e2322ae
Matteo Sarrecchia <morrea@hotmail.it>
2023-05-03 17:50:24 +0200

    modifiche struttura

--- ESERCIZIO EMAIL/js/main.js
+++ ESERCIZIO EMAIL/js/main.js
@@ -3,6 +3,7 @@
 // variabili globali
 const container = document.getElementById("container");
 let tipoEmail;
+let puoiEntrare = "Email NON presente nella lista, NON puoi entrare.";
 
 // chiedere email 
 const email = prompt("Inserisci la tua email");
@@ -11,21 +12,20 @@ console.log(email);
 // crea array con lista email della gente che puo accedere
 const listEmail = ["ciao", "ciao@gmail.it", "matteo@gmail.it", "viola@gmail.it", "andrea@gmail.it"]
 
+
 // verificare se l'email è presente nella lista
 for (let i = 0; i < listEmail.length; i++){
 
     tipoEmail = listEmail[i];
 
+    // condizione
     if (email === tipoEmail){
 
-        console.log("ok ci sta");
-        const ris=document.createElement("div");
-        container.append(ris);
-        ris.append("Email presente nella lista, puoi entrare.")
-    
-    } else console.log("email non esiste");
-        const ris=document.createElement("div");
-        container.append(ris);
-        ris.append("Email NON presente nella lista, NON puoi entrare.")
+    puoiEntrare= "Email presente nella lista, puoi entrare.";
 }
+}
+
+    const ris=document.createElement("div");
+    container.append(ris);
+    ris.append(puoiEntrare);
 
